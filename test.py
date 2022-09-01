4d8dfe536085fd0868801cdd2960af6978036b43
yj-melissa <melimelame@gmail.com>
2022-09-01 23:39:25 +0900

    220901 solved

--- test.py
+++ test.py
@@ -1,9 +1,18 @@
-N, X = map(int, input().split())
-A = list(map(int, input().split()))
-res = []
+notes = list(map(int, input().split()))
+ref = list(enumerate('cdefgabC', start=1))
 
-for i in A:
-    if i < X:
-        res.append(i)
+asc_cnt = des_cnt = 0
 
-print(*res)
+for idx, note in ref:
+    if idx == notes[idx-1]:
+        asc_cnt += 1
+    elif 9-idx == notes[idx-1]:
+        des_cnt += 1
+    else:
+        print('mixed')
+        break
+else:
+    if asc_cnt == 8:
+        print('ascending')
+    else:
+        print('descending')
