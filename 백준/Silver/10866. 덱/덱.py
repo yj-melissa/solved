6af45d09c71c828dfa03c819f96ded7520f90dfb
yj-melissa <109330621+yj-melissa@users.noreply.github.com>
2023-02-23 00:01:33 +0900

    Update 덱.py

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,39 +1,63 @@
 import sys
 
-lst = []
-N = int(sys.stdin.readline())
-for i in range(N):
-    data = list(sys.stdin.readline().split())
-    # print(data)
+N = int(input())        # 주어지는 명령의 수
+deque = [0] * N
+front = end = 0
 
-    if data[0] == 'push_back':
-        lst.append(int(data[1]))
-    elif data[0] == 'push_front':
-        lst.insert(0, int(data[1]))
-    elif data[0] == 'front':
-        if len(lst) == 0:
+for i in range(N):      # 명령 수만큼 반복
+    order = list(sys.stdin.readline().split())        # input() 쓰면 시간초과. readline() 쓰면 통과함.
+    order_word = order[0]
+
+    if order_word == 'push_front':
+        num = int(order[1])
+        if front == end:
+            deque[front] = num
+            end += 1
+        elif 0 < front < end:
+            deque[front - 1] = num
+            front -= 1
+        else:
+            deque.insert(front, num)
+            end += 1
+
+    elif order_word == 'push_back':
+        num = int(order[1])
+        deque[end] = num
+        end += 1
+
+    elif order_word == 'pop_front':
+        if front == end:
             print(-1)
         else:
-            print(lst[0])
-    elif data[0] == 'back':
-        if len(lst) == 0:
+            print(deque[front])
+            front += 1
+
+    elif order_word == 'pop_back':
+        if front == end:
             print(-1)
         else:
-            print(lst[-1])
-    elif data[0] == 'size':
-        print(len(lst))
-    elif data[0] == 'empty':
-        if len(lst) == 0:
+            end -= 1
+            print(deque[end])
+
+    elif order_word == 'size':
+        print(end - front)
+
+    elif order_word == 'empty':
+        if front == end:
             print(1)
         else:
             print(0)
-    elif data[0] == 'pop_front':
-        if len(lst) == 0:
+
+    elif order_word == 'front':
+        if (front == end) or (front == -1):
             print(-1)
         else:
-            print(lst.pop(0))
-    elif data[0] == 'pop_back':
-        if len(lst) == 0:
-            print(-1)
-        else:
-            print(lst.pop(-1))
+            print(deque[front])
+
+    elif order_word == 'back':
+            if front == end:
+                print(-1)
+            else:
+                print(deque[end - 1])
+
+
