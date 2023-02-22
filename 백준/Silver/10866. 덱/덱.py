4b214811ac840088fc9ea44b00d1415aad7dafca
yj-melissa <109330621+yj-melissa@users.noreply.github.com>
2023-02-22 23:39:15 +0900

    [Silver IV] Title: 덱, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,45 +1,65 @@
 import sys
 
-N = int(input())
-deque = []
+N = int(input())        # 주어지는 명령의 수
+deque = [0] * N
+global front, end
+front = end = 0
 
-for _ in range(N):
+for i in range(N):      # 명령 수만큼 반복
+    # order = list(input().split())
     order = list(sys.stdin.readline().split())
-    if order[0] == 'push_front':
-        deque = [int(order[1])] + deque
+    order_word = order[0]
 
-    elif order[0] == 'push_back':
-        deque.append(int(order[1]))
-
-    elif order[0] == 'pop_front':
-        if deque:
-            print(deque.pop(0))
+    if order_word == 'push_front':
+        num = int(order[1])
+        if front == end:
+            deque[front] = num
+            end += 1
+        elif 0 < front < end:
+            deque[front - 1] = num
+            front -= 1
         else:
+            deque.insert(front, num)
+            end += 1
+
+    elif order_word == 'push_back':
+        num = int(order[1])
+        deque[end] = num
+        end += 1
+
+    elif order_word == 'pop_front':
+        if front == end:
             print(-1)
-
-    elif order[0] == 'pop_back':
-        if deque:
-            print(deque.pop(-1))
         else:
+            print(deque[front])
+            front += 1
+
+    elif order_word == 'pop_back':
+        if front == end:
             print(-1)
-
-    elif order[0] == 'size':
-        print(len(deque))
-
-    elif order[0] == 'empty':
-        if deque:
-            print(0)
         else:
+            end -= 1
+            print(deque[end])
+
+    elif order_word == 'size':
+        print(end - front)
+
+    elif order_word == 'empty':
+        if front == end:
             print(1)
-
-    elif order[0] == 'front':
-        if deque:
-            print(deque[0])
         else:
-            print(-1)
+            print(0)
 
-    elif order[0] == 'back':
-        if deque:
-            print(deque[-1])
-        else:
+    elif order_word == 'front':
+        if (front == end) or (front == -1):
             print(-1)
+        else:
+            print(deque[front])
+
+    elif order_word == 'back':
+            if front == end:
+                print(-1)
+            else:
+                print(deque[end - 1])
+
+
