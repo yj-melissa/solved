9804fc39aacb241127b1d3b2af2b4c4eea6e95cf
yj-melissa <109330621+yj-melissa@users.noreply.github.com>
2023-02-27 22:29:45 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,35 +1,25 @@
-def push(x):
-    global top
-    top += 1
-    stack[top] = x
-
-def pop(x):
-    global top
-    top -= 1
-    return stack[top+1]
-
+# 9012_괄호
 
 T = int(input())
 for _ in range(T):
-    string = list(input())
-    stack = [0] * 50    # 문자열의 최대 길이 50
+    stack = [0] * 50        # 문자열 길이(2 <= x <= 50)
     top = -1
-    flag = 0    # YES: 0, NO; 1
+    string = input()
+    answer = "YES"
+
     for s in string:
         if s == '(':
-            push(s)
-        else:
-            if top == -1:    # 스택이 비어있는 상태라면 실패
-                flag = 1
-                break
-            elif stack[top] == '(':    # 이미 열린 괄호가 들어 있는 상태라면 ㅇㅋ
-                pop(s)
-                continue
-    else:
-        if top > -1:    # 검사를 끝냈는데 남아있는 괄호가 있다면 실패
-            flag = 1
+            top += 1
+            stack[top] = '('
 
-    if flag:
-        print('NO')
-    else:
-        print('YES')
+        elif s == ')':
+            if top >= 0:
+                top -= 1
+            else:               # 닫는 괄호가 여는 괄호보다 많으면 문자열 x
+                answer = "NO"
+                break
+
+    if top != -1 :        # 문자열이 끝났을 때 스택이 남아있으면 문자열x
+        answer = "NO"
+
+    print(answer)
