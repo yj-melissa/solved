4e967623b12f0e074b23dcc42754cf7b3e26c0fc
yj-melissa <109330621+yj-melissa@users.noreply.github.com>
2023-04-09 19:47:11 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 808 ms, Memory: 137548 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,19 +1,18 @@
-cards_minus = [0] * 10000001
-cards_plus = [0] * 10000001
-
 N = int(input())        # 상근이가 가지고 있는 카드 갯수
-cards = list(map(int, input().split()))      # 상근이가 가진 카드 목록
-for c in cards:
-    if c >= 0:
-        cards_plus[c] += 1
+have_cards = list(map(int, input().split()))        # 상근이가 가진 카드 목록
+card_dict = dict()
+
+for c in have_cards:
+    if c in card_dict:
+        card_dict[c] += 1
     else:
-        cards_minus[-c] += 1
+        card_dict[c] = 1
 
 M = int(input())        # 찾아야할 카드 갯수
 find_cards = list(map(int, input().split()))        # 찾아야할 카드 목록
 
 for f in find_cards:
-    if f >= 0:
-        print(cards_plus[f], end=' ')
+    if f in card_dict:
+        print(card_dict[f], end=' ')
     else:
-        print(cards_minus[-f], end=' ')
+        print(0, end= ' ' )
