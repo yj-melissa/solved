97f2c7c26ee78a7342c4a646ac4ec85b26676c67
yj-melissa <109330621+yj-melissa@users.noreply.github.com>
2023-07-26 10:44:54 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.32 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int price) {
+        double answer = 0;
+        if (price >= 500_000) {
+            answer = (double) price * 0.8;
+        } else if (price >= 300_000) {
+            answer = (double) price * 0.9;
+        } else if (price >= 100_000) {
+            answer = (double) price * 0.95;
+        } else {
+            answer = price;
+        }
+        return (int) Math.floor(answer);
+    }
+}
