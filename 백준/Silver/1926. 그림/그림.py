182af0da42109b6c8c758376f85ca08d1202a3b5
yj-melissa <109330621+yj-melissa@users.noreply.github.com>
2024-03-28 20:18:15 +0900

    [Silver I] Title: 그림, Time: 436 ms, Memory: 35588 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,5 +1,4 @@
-import sys
-sys.setrecursionlimit(10**7)
+from collections import deque
 
 def is_visited(row, col):
     if visited[row][col] == 1:
@@ -7,49 +6,63 @@ def is_visited(row, col):
     else:
         return False
 
-def is_in_range(row, col):
-    global N, M
-    if 0 <= row < N and 0 <= col < M:
+def is_blank(row, col):
+    if paper[row][col] == 0:
         return True
     else:
         return False
 
-def dfs(row, col, area):
-    if is_visited(row, col):
-        return area
+def is_out_of_range(row, col):
+    global N, M
+    if (0 <= row < N) and (0 <= col < M):
+        return False
     else:
-        visited[row][col] = 1       # 방문처리
+        return True
 
-    if info[row][col] == 0:
-        return area
+def bfs(row, col, area):
+    global max_area
+    queue = deque()
+    queue.append((row, col))
 
-    delta = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 우하좌상
+    visited[row][col] = 1       # 방문 처리
 
-    for dr, dc in delta:
-        nr = row + dr
-        nc = col + dc
+    while queue:
+        cr, cc = queue.popleft()
+        delta = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # 우하좌상
+        for dr, dc in delta:
+            nr = cr + dr
+            nc = cc + dc
 
-        if not is_in_range(nr, nc):
-            continue
+            if is_out_of_range(nr, nc):
+                continue
 
-        area = dfs(nr, nc, area)
+            if is_visited(nr, nc):
+                continue
 
-    return area + 1
+            if is_blank(nr, nc):
+                continue
+
+            queue.append((nr, nc))
+            visited[nr][nc] = 1
+            area += 1
+
+    return area
 
 
 N, M = map(int, input().split())
-info = [list(map(int, input().split())) for _ in range(N)]
+paper = [list(map(int, input().split())) for _ in range(N)]
 visited = [[0] * M for _ in range(N)]
 
-cnt = 0         # 그림의 개수
-max_area = 0    # 가장 넓은 넓이
+cnt = 0
+max_area = 0
 
 for row in range(N):
     for col in range(M):
-        if not is_visited(row, col) and info[row][col] == 1:
-            cnt += 1
-            area = dfs(row, col, 0)
-            if area > max_area:
-                max_area = area
+        if is_visited(row, col) or is_blank(row, col):
+            continue
+        cnt += 1
+        area = bfs(row, col, 1)
+        if area > max_area:
+            max_area = area
 
 print(cnt, max_area)
