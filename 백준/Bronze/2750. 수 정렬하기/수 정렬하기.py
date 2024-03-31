8492d56603966324c22023333c18e67cab15c1aa
yj-melissa <109330621+yj-melissa@users.noreply.github.com>
2024-03-31 23:16:54 +0900

    [Bronze II] Title: 수 정렬하기, Time: 104 ms, Memory: 31260 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,14 +1,35 @@
-def ordering():
-    for i in range(N-1):
-        min_idx = i
-        for j in range(i+1, N):
-            if nums[j] < nums[min_idx]:
-                min_idx = j
-        nums[min_idx], nums[i] = nums[i], nums[min_idx]
+import sys
+sys.setrecursionlimit(10**6)
 
+def quick_sort(start, end):
+    if start >= end:
+        return
+
+    pivot = start
+    left = start + 1
+    right = end
+
+    while left <= right:
+        while left <= end and nums[left] <= nums[pivot]:
+            left += 1
+        while right > start and nums[right] >= nums[pivot]:
+            right -= 1
+
+        if left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+        else:
+            nums[start], nums[right] = nums[right], nums[start]
+
+    quick_sort(start, right - 1)
+    quick_sort(right + 1, end)
 
 N = int(input())
-nums = [int(input()) for _ in range(N)]
-ordering()
+nums = [0] * N
+
+for i in range(N):              # 수 입력
+    nums[i] = int(input())
+
+quick_sort(0, N - 1)
+
 for i in range(N):
     print(nums[i])
