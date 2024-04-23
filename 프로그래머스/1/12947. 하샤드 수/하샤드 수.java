5ba46aa5b07a88c3ae6077ce33646c2bb8384cd7
yj-melissa <109330621+yj-melissa@users.noreply.github.com>
2024-04-23 23:12:52 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int tmp = x;
+        int sum = 0;
+        while (tmp > 0) {
+            sum += tmp % 10;
+            tmp /= 10;
+        }
+        if (x % sum != 0) {
+            answer = false;
+        }
+        return answer;
+    }
+}
