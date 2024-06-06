fa2a38715486a2730f7c8e8cd7e0410bdd87fd02
yj-melissa <109330621+yj-melissa@users.noreply.github.com>
2024-06-07 00:03:36 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 3084 ms, Memory: 37628 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -5,16 +5,11 @@ stack = []
 
 for i in range(N):
     while stack:
-        j = stack.pop()
-        if buildings[j] <= buildings[i]:        # 지금 건물이 더 높거나 같음
-            answer += (i - j - 1)
+        if buildings[stack[-1]] <= buildings[i]:
+            stack.pop()
         else:
-            stack.append(j)
             break
+    answer += len(stack)
     stack.append(i)
-
-while stack:
-    i = stack.pop()
-    answer += (N - 1 - i)
 
 print(answer)
